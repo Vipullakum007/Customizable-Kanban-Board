3689e0301560bd8991e592124e9e9619a8f414d6
Vipul_Lakum <lakumvipul6351@gmail.com>
2024-10-15 09:33:25 +0530

    task component added

--- kanban board/src/components/Task.jsx
+++ kanban board/src/components/Task.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Task = () => {
-  return (
-    <div>Task</div>
-  )
+const Task = ({task , columnId}) => {
+
+    const hadleDragStart = (e) => {
+        e.dataTransfer.setData('taskId', task.id);
+        e.dataTransfer.setData('fromColumnId', columnId);
+    }
+
+    return (
+        <div className='task' draggable onDragStart={hadleDragStart}>
+            <h2>Task</h2>
+            <p>This is a sample task.</p>
+        </div>
+    )
 }
 
 export default Task
